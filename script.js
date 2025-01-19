0162378a859680ee06327940a9ce813d76c53070
Leah <leahyuriwang@gmail.com>
2025-01-18 23:21:45 -0500

    working mass and cass map

---
+++ script.js
@@ -0,0 +1,7 @@
+let map = L.map('map').setView([42.33262, -71.07231], 18);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
