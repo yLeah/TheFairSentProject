7540189bc0e5559fdc0cfd54958306150d868191
Leah <leahyuriwang@gmail.com>
2025-01-19 04:24:11 -0500

    left right articles finished

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # The FairSent Project
 
-This is an interactive map of Mass and Cass, an area around the intersection of Melenea Cass Boulevard and Massachusetts Avenue. 
+This is an interactive map of Mass and Cass, the area around the intersection of Melenea Cass Boulevard and Massachusetts Avenue. 
--- script.js
+++ script.js
@@ -154,4 +154,15 @@ document.addEventListener('DOMContentLoaded', function () {
             map.removeLayer(orangeMarkers);
         }
     });
+
+    function updateTime() {
+        const date = new Date();
+        const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+        const time = date.toLocaleTimeString();
+        const day = date.toLocaleDateString(undefined, options);
+        document.getElementById('time').innerHTML = 
+        `Today is: ${day}, Current Time: ${time}`;
+    }
+    setInterval(updateTime, 1000);
+    updateTime();
 })
